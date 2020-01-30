d5d74089fd542dd5c3cd2a5839396ee575ef80fe
vcoras <vlad.coras@plentydevelopment.com>
2020-01-31 00:37:49 +0200

    Added skeleton of database handler

---
+++ src/main/java/org/apache/camel/example/xmldb/util/Database.java
@@ -0,0 +1,31 @@
+/**
+ * This class should only be able to handle 
+ * reading and writing of an .xml file
+ * 
+ * TODO: Include some form of security
+ */
+
+package org.apache.camel.example.xmldb.util;
+
+import java.io.File;
+import java.io.IOException;
+
+ public class Database {
+
+    private String filePath = "";
+    
+    // Constructors
+    public Database() { this.filePath = "database/default.xml"; }
+    public Database(String filePath) { this.filePath = filePath; }
+
+    public File read() {
+        File inputFile = new File(this.filePath);
+
+        return inputFile;
+    }
+
+    // For debugging purposes
+    public static void main(String[] args) {
+
+    }
+ }
