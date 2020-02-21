2b5850d6eec775fa558cd37e2385c075d2ad8fd9
vcoras <vlad.coras@plentydevelopment.com>
2020-02-21 14:11:24 +0200

    Added CRUD routes for objects

--- src/main/java/org/apache/camel/example/xmldb/TheRouteBuilder.java
+++ src/main/java/org/apache/camel/example/xmldb/TheRouteBuilder.java
@@ -1,6 +1,7 @@
 package org.apache.camel.example.xmldb;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.example.xmldb.objects.*;
 import org.apache.camel.model.rest.RestBindingMode;
 
 /**
@@ -19,23 +20,44 @@ public class TheRouteBuilder extends RouteBuilder {
         // REST API routes
         rest("/api").consumes("application/json").produces("application/json")
             
-            // Authors
-            .get("/author/{id}}").outType(Author.class)
-                .to("bean:authorService?method=getAuthorByID(${id})")
+            // Author
+            .get("/authors").outType(Author.class).to("bean:authorService?method=get()")
+            .get("/author/{id}").outType(Author.class).to("bean:authorService?method=getByID(${id})")
 
-            .put("/author/").outType(Author.class)
-                .to("bean:authorService?method=getAuthorByID(${id})")
-                
-            .get("/author").route().transform().constant("Hello World");
+            .post("/author").outType(Author.class).to("bean:authorService?method=create()")
+            .put("/author/{id}").outType(Author.class).to("bean:authorService?method=update(${id})")
+            .delete("/author/{id}").outType(Author.class).to("bean:authorService?method=delete(${id})")
+
+            // Article
+            .get("/articles").outType(Author.class).to("bean:articleService?method=get()")
+            .get("/article/{id}").outType(Author.class).to("bean:articleService?method=getByID(${id})")
+
+            .post("/article").outType(Author.class).to("bean:articleService?method=create()")
+            .put("/article/{id}").outType(Author.class).to("bean:articleService?method=update(${id})")
+            .delete("/article/{id}").outType(Author.class).to("bean:articleService?method=delete(${id})")
+
+            // Department
+            .get("/departments").outType(Author.class).to("bean:departmentService?method=get()")
+            .get("/department/{id}").outType(Author.class).to("bean:departmentService?method=getByID(${id})")
+
+            .post("/department").outType(Author.class).to("bean:departmentService?method=create()")
+            .put("/department/{id}").outType(Author.class).to("bean:departmentService?method=update(${id})")
+            .delete("/department/{id}").outType(Author.class).to("bean:departmentService?method=delete(${id})")
             
+            // Faculty
+            .get("/faculties").outType(Author.class).to("bean:facultyService?method=get()")
+            .get("/faculty/{id}").outType(Author.class).to("bean:facultyService?method=getByID(${id})")
+
+            .post("/faculty").outType(Author.class).to("bean:facultyService?method=create()")
+            .put("/faculty/{id}").outType(Author.class).to("bean:facultyService?method=update(${id})")
+            .delete("/faculty/{id}").outType(Author.class).to("bean:facultyService?method=delete(${id})")
             
+            // University
+            .get("/universities").outType(Author.class).to("bean:universityService?method=get()")
+            .get("/university/{id}").outType(Author.class).to("bean:universityService?method=getByID(${id})")
 
-            
-
-
-
-
-        // .get("/user/{id}}").outType(User.class)
-        //     .to("bean:userService?method=getUser(${header.id})")
+            .post("/university").outType(Author.class).to("bean:universityService?method=create()")
+            .put("/university/{id}").outType(Author.class).to("bean:universityService?method=update(${id})")
+            .delete("/university/{id}").outType(Author.class).to("bean:universityService?method=delete(${id})");
     }
 }
